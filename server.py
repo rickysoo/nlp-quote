8f5ff36401049cad1ca566b44a56924c3552fca9
Ricky Soo <8564646+rickysoo@users.noreply.github.com>
2022-08-22 22:42:00 +0800

    Add server.py

---
+++ server.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import tensorflow
+
+from tensorflow.keras import preprocessing as kprocessing
+from tensorflow.keras.models import load_model
+
+import nltk
+from nltk.tokenize import sent_tokenize
+
+import pickle
+
+from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template, request, jsonify
+
+sentences_test = [
+  'A journey of a thousand miles begins with a single step.',
+  'Are you beginning a journey of a thousand steps tomorrow?',
+  'The Post cited people familiar with the investigation as saying that federal agents were looking for classified documents related to nuclear weapons',
+  'But it turns out that Trump did take such material from the White House.',
+  'No one will reap except what they sow.'
+]
+
+model = load_model('rnn.h5')
+corpus = pickle.load(open('corpus.model', 'rb'))
+
+max_words = 30000
+max_len = 200
+
+tokenizer = kprocessing.text.Tokenizer(lower=True, split=' ', num_words=max_words, oov_token="<pad>", filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n')
+tokenizer.fit_on_texts(corpus)
+
+
+def sent_predict(sentences, tokenizer=tokenizer, threshold=0.5):
+  seq = kprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences(sentences), maxlen=max_len)
+  predictions = model.predict(seq).reshape(-1)
+  predictions = dict(zip(sentences, predictions))
+  predictions = { key:float(value) for (key, value) in predictions.items() if value >= threshold }
+  return predictions
+
+
+def extract_quotes(text, threshold=0.5):
+  return sent_predict(sent_tokenize(text), threshold=threshold)
+
+output_test = sent_predict(sentences_test)
+
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  return jsonify(output_test)
+
+@app.route('/api', methods=['POST'])
+def api():
+  if request.form.get('data'):
+    data = request.form['data']
+    threshold = 0.5
+
+    if request.form.get('threshold'):
+      th = request.form['threshold']
+
+      try:
+        th = float(th)
+        
+        if th >= 0.0 and th <= 1.0:
+          threshold = th
+
+      except ValueError:
+        pass
+
+    quotes = extract_quotes(data, threshold=threshold)
+    return quotes
+  else:
+    return 'Please include text in the POST request.', 401
+
+if __name__ == '__main__':
+  app.run()
